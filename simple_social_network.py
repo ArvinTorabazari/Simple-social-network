6f2023bf0728828842a5a77632b875de5d2d6c7f
Arvin Torabazari <atorabaz@umd.edu>
2023-10-30 21:16:58 -0400

    added simple_social_network.py

---
+++ simple_social_network.py
@@ -0,0 +1,9 @@
+""" This is a simple social network for tracking connections between people"""
+
+class Person():
+    """ A person in the social network
+    Attributes:
+        name (str): a name of a person
+        connections(set of person): Other people in the social network who know
+        this person
+    """
